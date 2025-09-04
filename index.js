0506736952e0cc5e30115b23a43b7f2d4caccefa
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-09-05 03:11:01 +0600

    backend setup completed

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
